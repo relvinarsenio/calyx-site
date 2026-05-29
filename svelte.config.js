efe16ad2f999cb5b2be371b4b236f49ae9d9c4d0
Alfie Ardinata <88470966+relvinarsenio@users.noreply.github.com>
2026-05-29 19:01:40 +0000

    chore: comment out inlineStyleThreshold in svelte config
    Signed-off-by: Alfie Ardinata <88470966+relvinarsenio@users.noreply.github.com>

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		inlineStyleThreshold: 32768
+		// inlineStyleThreshold: 32768
 	}
 };
 
