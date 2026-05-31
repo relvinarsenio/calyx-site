21ee78688aad00b7e6b364592095aeaa19178dad
Alfie Ardinata <88470966+relvinarsenio@users.noreply.github.com>
2026-05-31 17:40:54 +0000

    fix: remove redundant verification token route
    Signed-off-by: Alfie Ardinata <88470966+relvinarsenio@users.noreply.github.com>

--- src/routes/[verification=google_verify]/+server.ts
+++ src/routes/[verification=google_verify]/+server.ts
@@ -4,14 +4,13 @@ import type { RequestHandler, EntryGenerator } from './$types';
 export const prerender = true;
 
 export const entries: EntryGenerator = () => {
-	return [{ verification: GOOGLE_VERIFY_TOKEN }, { verification: `${GOOGLE_VERIFY_TOKEN}.html` }];
+	return [{ verification: `${GOOGLE_VERIFY_TOKEN}.html` }];
 };
 
 export const GET: RequestHandler = ({ params }) => {
 	const filename = params.verification;
-	const cleanFilename = filename.endsWith('.html') ? filename : `${filename}.html`;
 
-	return new Response(`google-site-verification: ${cleanFilename}`, {
+	return new Response(`google-site-verification: ${filename}`, {
 		headers: {
 			'content-type': 'text/html; charset=utf-8'
 		}
