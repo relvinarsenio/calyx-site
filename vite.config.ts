1b184bcb51dccf819ca4171df13eb674ef480d7a
Alfie Ardinata <88470966+relvinarsenio@users.noreply.github.com>
2026-08-01 14:59:45 +0000

    feat: integrate vite-plugin-image-optimizer to automate asset optimization
    Signed-off-by: Alfie Ardinata <88470966+relvinarsenio@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), ViteImageOptimizer()],
 	define: {
 		__BUILD_DATE__: JSON.stringify(new Date().toISOString().split('T')[0])
 	}
